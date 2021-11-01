3e8686a5e99a5e2b7819776fac58319d6bce9565
LXR YOUTUBER AMAL <90690553+LXR-YOUTUBER-AMAL@users.noreply.github.com>
2021-11-01 09:01:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -529,7 +529,7 @@ async def auto_filter(client, message):
             for file in files:
                 file_id = file.file_id
                 btn.append(
-                    [InlineKeyboardButton(text=f"{file.file_name} {get_size(file.file_size)}", callback_data=f'files#{file_id}')]
+                    [InlineKeyboardButton(text=f"🎬 [{get_size(file.file_size)}] 📂 {file.file_name}", callback_data=f'files#{file_id}')]
                     )
         if not btn:
             return
